7d1e14579f0e733f96303fbeffc5931ca0c516af
Kaivalya Naik <kaivalya.naik@pubmatic.com>
2025-03-09 07:21:42 +0530

    Stage 1 --> 7testcases completed.

--- shell.c
+++ shell.c
@@ -4,43 +4,77 @@
 #include<unistd.h>
 #include<sys/wait.h>
 
+// Structures
+typedef struct Command
+{
+    char *command;
+    char *operator;
+} Command;
+
 // Function prototypes
 void cash();
 char** splitinput(char *buff);
 int executecmd(char** tokens);
 int checkAndExecuteInbuiltFunctions(char** tokens);
-char** getCommands(char* buff);
+Command* parseInput(char *buff, int *ncom);
 
-struct Command
-{
-    char *command;
-    char *operator;
-};
 
+
+char* trim(char* str) {
+    while (*str == ' ') str++;  
+    char* end = str + strlen(str) - 1;
+    while (end > str && *end == ' ') end--;  
+    *(end + 1) = '\0';  
+    return str;
+}
 
 
 void cash(){
     while(1){
-        printf("$");
+        printf("$ ");
         fflush(stdout);
         char buff[1024];
         if(fgets(buff,1024,stdin)==NULL)
             exit(EXIT_SUCCESS);
         
-        char** commands=getCommands(buff);
+        int n;
+        Command * commands = parseInput(buff,&n);
         int i = 0;
-        while(commands[i]!=NULL){
-            printf("Command :%s",commands[i]);
-            char** tokens=splitinput(commands[i]);
-            int nextCmd;
-            if(tokens!=NULL){
-                nextCmd = executecmd(tokens);
+        int prevCmd = 1;  // Initialize to success for first command
+        
+        while(i<n){
+            if(i == 0 || (i > 0 && commands[i-1].operator == NULL)) {
+                char** tokens = splitinput(commands[i].command);
+                prevCmd = executecmd(tokens);
+                free(tokens); 
             }
-            free(tokens);
+            else if(commands[i-1].operator != NULL) {
+                if(strcmp(commands[i-1].operator,"&&") == 0) {
+                    if(prevCmd == 1) {
+                        char** tokens = splitinput(commands[i].command);
+                        prevCmd = executecmd(tokens);
+                        free(tokens);
+                    }
+                }
+                else if(strcmp(commands[i-1].operator,"||") == 0) {
+                    if(prevCmd == 0) {
+                        char** tokens = splitinput(commands[i].command);
+                        prevCmd = executecmd(tokens);
+                        free(tokens);
+                    }
+                }
+            }
             i++;
         }
-        free(commands);
         
+        // Free all commands and operators
+        for(i = 0; i < n; i++){
+            free(commands[i].command);
+            if(commands[i].operator != NULL) {
+                free(commands[i].operator);
+            }
+        }
+        free(commands);
     }
 }
 
@@ -50,22 +84,52 @@ int main(int argc,char **argv){
 }
 
 
-char ** getCommands(char *buff){
-    int position=0;
-    char **commands = malloc(1024* sizeof(char*));
-    char* com;
-
-    buff[strcspn(buff, "\n")] = 0;
-    com = strtok(buff,";");
-    while(com!=NULL){
-        commands[position] = com;
-        position++;
-        com = strtok(NULL,";");
+Command* parseInput(char *buff, int *ncom) {
+    int pos = 0;
+    int commandCount = 0;
+    Command *commands = malloc(1024 * sizeof(Command));
+    char *token;
+    char *start = buff;
+    
+    while (buff[pos] != '\0') {
+        // Find the end of the command
+        start = &buff[pos];
+        while (buff[pos] != '\0' && buff[pos] != ';' && 
+               !(buff[pos] == '&' && buff[pos + 1] == '&') && 
+               !(buff[pos] == '|' && buff[pos + 1] == '|')) {
+            pos++;
+        }
+        
+        // Extract and store the command
+        int cmdLen = &buff[pos] - start;
+        token = malloc(cmdLen + 1);
+        strncpy(token, start, cmdLen);
+        token[cmdLen] = '\0';
+        commands[commandCount].command = trim(token);
+        
+        // Check the operator type
+        if (buff[pos] == '\0') {
+            commands[commandCount].operator = NULL;
+        } else if (buff[pos] == ';') {
+            commands[commandCount].operator = NULL;
+            pos++;
+        } else if (buff[pos] == '&' && buff[pos + 1] == '&') {
+            commands[commandCount].operator = strdup("&&");
+            pos += 2;
+        } else if (buff[pos] == '|' && buff[pos + 1] == '|') {
+            commands[commandCount].operator = strdup("||");
+            pos += 2;
+        }
+        
+        // Skip whitespace after operator
+        while (buff[pos] == ' ') pos++;
+        
+        commandCount++;
     }
-    commands[position]=NULL;
+    
+    *ncom = commandCount;
     return commands;
 }
-
 
 
 char** splitinput(char *buff){
@@ -105,11 +169,14 @@ int executecmd(char** tokens){
     if(pid == 0){
         if(execvp(tokens[0],tokens) == -1){
             perror("Command Failed");
+            exit(EXIT_FAILURE);
         }
         exit(EXIT_FAILURE);
     }
     else{
-        waitpid(pid,NULL,0);
+        int status;
+        waitpid(pid, &status, 0);
+        return WIFEXITED(status) && WEXITSTATUS(status) == 0;
     }
 }
 
@@ -126,6 +193,20 @@ int checkAndExecuteInbuiltFunctions(char** tokens){
             if(chdir(tokens[1])!= 0){
                 perror("cd failed");
             }
+        }
+        return 1;
+    }
+    else if(strcmp(tokens[0],"exec")==0){
+        if(tokens[1] == NULL){
+            fprintf(stderr, "exec: expected command\n");
+            return 1;
+        }
+        
+        char** args = &tokens[1];
+        
+        if(execvp(args[0], args) == -1){
+            perror("exec failed");
+            exit(EXIT_FAILURE);
         }
         return 1;
     }
